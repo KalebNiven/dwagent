163e02cf207cd7df3d3c01d1ffa46eb0257218c2
J0kerStar90914 <112364526+J0kerStar90914@users.noreply.github.com>
2022-10-18 22:18:07 +0530

    aaaasss

--- core/utils_core.py
+++ core/utils_core.py
@@ -27,7 +27,6 @@ _biswindows=(platform.system().lower().find("window") > -1)
 _bislinux=(platform.system().lower().find("linux") > -1)
 _bismac=(platform.system().lower().find("darwin") > -1)
 
-
 def is_py2():
     return sys.version_info[0]==2
 
